293a16a5cf309d8c8619ca27b9a86927f857d090
realhacker85 <realhacker85@gmail.com>
2026-03-12 18:02:39 +0300

    Add files via upload

---
+++ plugins/menu.js
@@ -0,0 +1,28 @@
+export const menu = (name)=>{
+
+return `
+╔══〔 REAL HACKER BOT 〕══╗
+👤 USER : ${name}
+👑 OWNER : RUZZER B47
+⚡ PREFIX : .
+
+📜 COMMANDS
+.menu
+.ping
+
+🛡 ETHICAL HACKING
+.portscan
+.whois
+.iplookup
+
+🔐 CYBERSECURITY
+.encrypt
+.decrypt
+
+⚙️ TOOLS
+.tts
+.translate
+
+BOT STATUS: ONLINE
+`
+}
